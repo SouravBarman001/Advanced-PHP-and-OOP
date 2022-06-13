2ed7e93b8a7332f79c301c050e6d0b2b39fa7437
Sourav <sourav.iit.nstu@gmail.com>
2022-06-13 22:07:27 +0600

    staticmethod

--- OOP/staticMathod.php
+++ OOP/staticMathod.php
@@ -1,12 +1,14 @@
 <?php
 
+// we have to declare self to access static member in this class
+
 // class Personal{
 //     public static $name = "Google";
     
-//     public $age = 21;
+//     public static $age = 21;
 
-//     public static function showName(){
-//         echo "age : 21";
+//     static function showName(){
+//         echo self::$age;
 //     }
 // }
 
@@ -15,18 +17,28 @@
 // Personal::showName();
 
 
-class greeting{
-    public static function welcome(){
-        echo "Hello world!";
-    }
-    public function __construct(){
-        self::welcome();
+// class greeting{
+//     public static function welcome(){
+//         echo "Hello world!";
+//     }
+//     public function __construct(){xsza
+//         self::welcome();
+//     }
+// }
+
+// new greeting();
+
+// access static propertises in inheritance 
+class A{
+    public static $name = "razu";
+
+}
+
+class B extends A{
+    public static function showInfo(){
+        echo A::$name;
     }
 }
 
-new greeting();
-
-
-
-
+B::showInfo();
 ?>
